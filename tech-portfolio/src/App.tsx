1450817990726423d84039caa754e5189c488f87
J M Adis <jessicaadis7@gmail.com>
2024-03-12 17:56:32 +0000

    reuploaded ProjectInfo types and updated app & stylings

--- tech-portfolio/src/App.tsx
+++ tech-portfolio/src/App.tsx
@@ -1,11 +1,22 @@
-import './App.css'
+import Nav from "./components/Nav/Nav";
+import About from "./components/About/About";
+import Skills from "./components/Skills/Skills";
+import PortfolioList from "./containers/PortoflioList/PortfolioList";
+import { Projects } from "./data/ProjectInfo";
+import Cv from "./components/CV/Cv";
+import Footer from "./components/Footer/Footer";
 
 function App() {
 
   return (
-    <>
-      
-    </>
+    <div>
+    <Nav />
+    <About />
+    <Skills />
+    <PortfolioList projects={Projects} />
+    <Cv />
+    <Footer />
+  </div>
   )
 }
 
---
+++ tech-portfolio/src/types/ProjectInfoTypes.ts
@@ -0,0 +1,8 @@
+export type ProjectInfoTypes = {
+    title: string; 
+    primarylanguage: string;
+    secondarylanguage?: string;
+    repo: string;
+    link?: string;
+    img: string;
+};
